97a2cd53ce9059711d0d8270f759b29e07438a44
Merve <ozermerve61@gmail.com>
2019-06-09 17:04:22 +0300

    Test Eklendi.

--- ConsoleMarsRover/Program.cs
+++ ConsoleMarsRover/Program.cs
@@ -11,6 +11,10 @@ namespace ConsoleMarsRover
     {
         static void Main(string[] args)
         {
+            //Planet ctora 2 servis veriyorum, input ve output yani şu anda input ve outputun Console dan işleyeceği bildirisini yapıyorum. 
+            //Bu tip temelde IInputService ve IOutputService interfacelerini implemente ettiklerinden, uygulama ileride input ve outputun 
+            //Console dan değil de bir json dosyasından yada bir webservisden yada bir database den alınması istendiğinde 
+            //tüm uygulamayı değiştirmeden programın başlangıç noktasından onu enjecte edebileyim. (Dependency Injection)
             Planet planet = new Planet(new ConsoleInputService(), new ConsoleOutputService());
             planet.CreateMap();
             planet.GenarateRovers();
